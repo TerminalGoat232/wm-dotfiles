d9423bf1c716b86d86ea00646d69d1e64613078e
0x4af <74200268+TerminalGoat232@users.noreply.github.com>
2023-02-24 23:43:08 +0700

    Delete space.py

--- dots/polybar/space.py
+++
@@ -1,9 +0,0 @@
-import argparse
-def spc():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("spc")
-    args = parser.parse_args()
-    spced = ''.join([" " * x for x in range(int(args.spc))])
-    return spced
-if __name__ == "__main__":
-    print(spc())
