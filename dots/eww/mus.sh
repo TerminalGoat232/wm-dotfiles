fa6f21bfc4ebaf0d2105cd8cb146979474ac7fde
0x4af <74200268+TerminalGoat232@users.noreply.github.com>
2023-02-24 23:07:50 +0700

    Update mus.sh

--- dots/eww/mus.sh
+++ dots/eww/mus.sh
@@ -1,30 +1,26 @@
 
+#!/bin/bash
 title(){
 	local tt
 	local tt_len
-	tt=`playerctl metadata --format ' {{ title }} '`
+	tt=`playerctl metadata --format ' {{ title }} ' 2>/dev/null`
 	tt_len=${#tt}
 	[[ ${tt_len} -gt 20 ]] && echo ${tt::$tt_len-($tt_len-20)} || echo ${tt}
-	[[ ${tt} == "No players found" ]] && echo "No players found" | eww close ma
 }
 title_and_stuff(){
-	local ttl,c,status,mm,i
-	ttl=`playerctl metadata --format ' {{ title }} '`
-# 	zy=`playerctl metadata --format ' {{ artist }} '`
-# 	zz=`playerctl metadata --format '{{ status }}'`
-	status=""
-	c=${#ttl}
-	if [[ ${zz} == "Paused" ]]; then
-		status="⠀⠀⠀⠀玲⠀⠀  ⠀⠀怜"
-	else status="⠀⠀⠀⠀玲⠀⠀  ⠀⠀怜"
-	fi
-	[[ ${#ttl} -gt 28 ]] && echo ${status} "\n" ${ttl::$c-($c-28)}"...⠀⠀⠀" || { 
-		for (( i=0;i<(28-${c});++i )); do 
+	local titl,titl_len,rtist,sttus,st,mm,i
+	titl=`playerctl metadata --format ' {{ title }} '`
+	#rtist=`playerctl metadata --format ' {{ artist }} '`
+	sttus=`playerctl metadata --format '{{ status }}'`
+	st=""
+	titl_len=${#titl}
+	[[ ${sttus} == "Paused" ]] && st="⠀玲⠀⠀  ⠀⠀怜" || st="⠀玲⠀⠀  ⠀⠀怜"
+	[[ ${#titl} -gt 28 ]] && echo ${titl_len} "\n"${titl::$titl_len-($titl_len-28)}"...⠀⠀⠀" || { 
+		for (( i=0;i<(32-${titl_len});++i )); do 
 			mm+="⠀" 
 		done 
-		echo ${status} "\n" ${ttl}${mm}
+		echo ${st} "\n"""${titl}${mm::$titl_len-($titl_len-18)}
 	}
-	
 }
 artist(){
 	local ar
@@ -33,12 +29,12 @@ artist(){
 	local m
 	ar=`playerctl metadata --format ' {{ artist }} '`
 	al=${#ar}
-	[[ ${al} -gt 12 ]] && echo "⠀ "${ar}"⠀" || echo "⠀"${ar}""
+	[[ ${al} -gt 12 ]] && echo "⠀⠀"${ar}"⠀" || echo "⠀"${ar}""
 }
 img(){
-	local m 
-	m=`playerctl metadata --format "{{mpris:artUrl}}" `
-	echo ${m:7}
+	local get_img_dir 
+	get_img_dir=`playerctl metadata --format "{{mpris:artUrl}}"`
+	echo ${get_img_dir:7}
 }
 
 if [[ "$1" == "--img" ]]; then
