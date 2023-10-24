7c9018a3d4807025e5ed66d07084aa7a2af11f64
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2023-10-25 00:38:05 +0700

    re-colored / simplified bar

--- dots/polybar/memory.py
+++ dots/polybar/memory.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import psutil
 from time import *
+un = 1028 << 20
 cc = [
-f"   {str(psutil.swap_memory().used/(2048**2+1024**3))[:6]}",
-f"/ {str(round(psutil.swap_memory().total/(2048**2+1024**3)))}GB",
-f" {str(psutil.virtual_memory().used*10**-6.02419)[:6]}",
-f"/ {str(psutil.virtual_memory().total*10**-6.02419)[:6]}MB  ", ]
+f"   {str(psutil.swap_memory().used/(un))[:6]} / {str(round(psutil.swap_memory().total/(un)))}GB "]
+#f" {str(psutil.virtual_memory().used/(un))[:3]} / {str(psutil.virtual_memory().total/(un))[:3]}GB "]
 for cock in cc:print(cock,end=" ");sleep(1)
