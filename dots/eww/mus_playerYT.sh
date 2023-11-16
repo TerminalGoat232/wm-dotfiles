e324f766d530789cdf92a86ea02f6ba9bfc15d06
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2023-11-16 14:52:58 +0700

    balls

--- dots/eww/mus_playerYT.sh
+++ dots/eww/mus_playerYT.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 v="$1"
 m() {  
- k=`python3 ~/.config/eww/ytsearch.py ${v} --yt-url`
+ k=`python3 ~/.config/eww/ytsearch.py ${v} `
  cvlc --play-and-exit ${k} --no-video --h264-fps=1
 }
 m
