76fd8a840c0d1302196703589b1cc8d1cd8b74b0
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2023-12-28 23:48:58 +0700

    added some goofy aliases

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -4,5 +4,21 @@ if status is-interactive
 end
 set -g fish_greeting
 #nitch
-neofetch --ascii ascii
+#neofetch --ascii ascii
+treefetch --bonsai 
 starship init fish | source
+
+thefuck --alias | source
+
+alias :3=echo
+alias nomnom=killall
+alias nv=nvim
+alias update='yay -Syu'
+alias clock='tty-clock -s -c 1'
+alias neofetch='neofetch --ascii ascii'
+alias cya=reboot
+alias kys='shutdown now'
+alias kleww='killall eww'
+alias qa='killall'
+alias rmpkg='yay -Rns'
+alias aged='./arch-age'
