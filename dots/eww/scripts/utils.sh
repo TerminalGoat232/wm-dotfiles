7a5566a2e086a7d323f6876c5292db0998b5592d
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2024-03-17 11:20:46 +0700

    y

---
+++ dots/eww/scripts/utils.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+swp_used=`free -m | grep "Swap" | awk '{print $3}'`
+swp=`free -m | grep "Swap" | awk '{print int(($2*0.001))}'`
+core_temp(){
+  echo $(sensors | grep "CPU" | awk '{print substr($2,2)}')
+}
+swap_used_perc(){
+  res=$((($swp_used/16)*100))
+  printf "%.0f\n" $res
+}
+
+swap_total(){
+ echo  $swp 
+}
+if [[ "$1" == "-c" ]]; then 
+  core_temp
+elif [[ "$1" == "-S" ]]; then 
+  swap_total
+elif [[ "$1" == "-sp" ]]; then 
+  swap_used_perc
+fi
