ac37f0059ce2d34d897cf2a23f2c2127d1c15141
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2024-03-17 12:24:40 +0700

    idk smh

--- dots/eww/input_handle_lc.sh
+++ dots/eww/input_handle_lc.sh
@@ -1,1 +1,3 @@
-sed -i "9d" $HOME/.config/polybar/tempout.py && sed -i "9i\region='$1'" $HOME/.config/polybar/tempout.py && eww close changeloc
+#!/bin/bash
+#sed -i "9d" $HOME/.config/polybar/tempout.py && sed -i "9i\region='$1'" $HOME/.config/polybar/tempout.py && eww close changeloc
+sed -i "2d" ~/.config/eww/scripts/weather_status.sh && sed -i "2i\region='$1'" ~/.config/eww/scripts/weather_status.sh && eww close changeloc
