080147025b69c01ad5057b7558cb02f55abe7267
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2025-01-27 22:59:34 +0700

    mor aliases idk

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -5,7 +5,7 @@ end
 set -g fish_greeting
 #nitch
 #neofetch --ascii ascii
-treefetch --bonsai 
+#treefetch --bonsai 
 starship init fish | source
 
 thefuck --alias | source
@@ -13,12 +13,18 @@ thefuck --alias | source
 alias :3=echo
 alias nomnom=killall
 alias nv=nvim
-alias update='yay -Syu'
+alias update='yay'
+alias pacstat='yay -Ps'
+alias ccache='sudo rm -rf /var/cache/pacman/pkg/*'
 alias clock='tty-clock -s -c 1'
-alias neofetch='neofetch --ascii ascii'
+alias neofetch='neofetch --ascii_distro arch_small'
 alias cya=reboot
 alias kys='shutdown now'
 alias kleww='killall eww'
 alias qa='killall'
 alias rmpkg='yay -Rns'
 alias aged='./arch-age'
+alias c='clear'
+alias pwdcp='pwd | tr -d "\n" | xclip -sel c'
+alias ...="cd ../.."
+set PATH "/home/terminalgoat/.local/bin:$PATH"
