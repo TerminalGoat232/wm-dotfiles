743adcd2b2072ae1b648c257de58adf590506be5
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2025-06-23 00:40:15 +0700

    handling

--- dots/eww/scripts/weather_status.sh
+++ dots/eww/scripts/weather_status.sh
@@ -5,61 +5,69 @@ time=$(date +%-H)
 dat_list=($data_source)
  
 # Some notation for each value in dat_list[i]
-#  i   |     1         2       3       4     |
-# data | real_temp feel_temp weather humid   |
+#  i   |     1         2       3       4   |
+# data | real_temp feel_temp weather humid |
 #  __  |     5       6      7     8    9     |
 #  __  |   wind   pressure  uv  moon precip  |
 
-check4Digits(){ 
+check4Digits(){ # <$1: STR> -> BOOL
   [[ "$1" =~ [0-9] ]] && 
     echo "true" || echo "false"
 }
 
-real_temp(){
-  has_digits=$(check4Digits ${dat_list[0]})
-  if [[ "$has_digits" == "true" ]]; then
-    echo ${dat_list[0]}
+echoExpect(){ # <$1:STR $2:ERR_STR> -> STR
+  has_digits=$(check4Digits "$1")
+  if [[ "$has_digits" == "true" && ${#1} -ge 1 ]]; then
+    echo "$1"
   else 
-    echo "OwO?"
+    echo "$2"
   fi
 }
 
+real_temp(){
+  echoExpect ${dat_list[0]} "OwO?"
+}
+
 feel_temp(){
-  echo ${dat_list[1]}
+  echoExpect ${dat_list[1]} "UwU?"
 }
 
 weather_cond(){
   wther_icons="⛅☁🌦⛈🌨🌩🌫🌧☀"
   wther_icons_replacement="󰅟󰙾󰼶󰖓"
   k=${dat_list[2]}
-  i=0
-  while [ $i -lt ${#wther_icons} ]; do
-    if [[ "$k" == *${wther_icons:${i}:1}* ]]; then  
-      printf ${wther_icons_replacement:${i}:1}
-    fi
-    i=$((i+1))
-  done 
+  if [[ ${#k} -lt 1 ]]; then 
+    echo " "
+  else 
+    i=0
+    while [ $i -lt ${#wther_icons} ]; do
+      if [[ "$k" == *${wther_icons:${i}:1}* ]]; then  
+        echo ${wther_icons_replacement:${i}:1}
+      fi
+      i=$((i+1))
+    done 
+  fi
 }
 humid(){
-  echo "%󰖌" ${dat_list[3]}
+  echoExpect "%󰖌 "${dat_list[3]} "n/a"
 }
 wind_speed(){
-  echo "" ${dat_list[4]}
+  echoExpect " "${dat_list[4]} "n/a"
 }
 air_pressure(){
-  echo "   " ${dat_list[5]}
+  echoExpect "    "${dat_list[5]} "n/a"
 }
 uv(){
-  echo "uv" ${dat_list[6]}
+  echoExpect "uv "${dat_list[6]} "n/a"
 }
 moon_phase(){
-  echo ${dat_list[7]}
+  echoExpect ${dat_list[7]} "n/a"
 }
 precipitation(){
-  echo "  " ${dat_list[8]}
+  echoExpect "   "${dat_list[8]} "n/a"
 }
 delta_t(){
-  echo "󰇂C" $(echo $data_source | awk '{print $2-$1}')
+  echo "󰇂C "$(echo $data_source | awk '{print $2-$1}')
 }
 time_session(){
   res='' 
@@ -72,7 +80,7 @@ time_session(){
   fi
   printf $res
 }
-#echo $data_source
+
 case $1 in
   "-t") real_temp ;;
   "-T") feel_temp ;;
